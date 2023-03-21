a0d3572cb838a726f406fdea55987b7c3f7aa69b
harishvarma7 <82836324+harishvarma7@users.noreply.github.com>
2023-03-21 17:35:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,58 @@ from pydantic import BaseModel
 
 app=FastAPI()
 
+
+class disease(BaseModel):
+    age:int
+    sex:int
+    cp:int
+    trestbps:int
+    chol:int
+    fbs:int
+    restecg:int
+    thalach:int
+    exang:int
+    oldpeak:int
+    slope:int
+    ca:int
+    thal:int
+    
+    
+app=FastAPI()
+
+mod=pickle.load(open('model.pkl','rb'))
+
 @app.get('/')
 def hello_world():
     return {'message': 'Hello, World'}
+    #return render_template("frontend/public/index.html")
+@app.post('/prediction')
+def prediction(data:disease):
+    data=data.dict()
+    #pars=[int(x) for x in  request.form.values()]
+    
+    #pars=[63,1,3,145,233,1,0,150,0,2,0,0,1]
+    age=data['age']
+    sex=data['sex']
+    cp=data['cp']
+    trestbps=data['trestbps']
+    chol=data['chol']
+    fbs=data['fbs']
+    restecg=data['restecg']
+    thalach=data['thalach']
+    exang=data['exang']
+    oldpeak=data['oldpeak'] 
+    slope=data['slope'] 
+    ca=data['ca'] 
+    thal=data['thal']
+    
+    
+    
+    preds=mod.predict([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+    print(preds)
+    #print(preds)
+    return str(preds)
+    #return render_template('heart.html',pred='Chance of getting a heart disease is {}'.format(preds))
 
 if __name__=='__main__':
      uvicorn.run(app,host="127.0.0.1",port=8000)
